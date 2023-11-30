b2c8c39ab7b6cc1ec1b1f31df7916e7891c70f5a
Sayangシ <117487048+FarizalA@users.noreply.github.com>
2023-12-01 00:36:27 +0700

    main.js
    Signed-off-by: Sayangシ <117487048+FarizalA@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+});
+$(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+        $(".custom-carousel .item").not($(this)).removeClass("active");
+        $(this).toggleClass("active");
+    });
+});
